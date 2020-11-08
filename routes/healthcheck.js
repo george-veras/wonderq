765f31e8986df2e122b80fd2613a1a25339e6cb0
George Véras Valentim <george.veras.valentim@gmail.com>
2020-11-08 14:02:43 -0300

    Some refac and fix

--- routes/healthcheck.js
+++ routes/healthcheck.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 import HealthCheckController from '../controllers/healthcheck-controller.js'
 
 const router = express.Router()
 
 // Prefix: /healthcheck
-router.get('/', HealthCheckController.status)
+router.get('/', asyncHandler(HealthCheckController.status))
 
 export default router
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ import MessagesRoutes from './messages.js'
 
 const router = express.Router()
 router.use('/healthcheck', HealthcheckRoutes)
-router.use('/message', MessagesRoutes)
+router.use('/messages', MessagesRoutes)
 
 export default router
