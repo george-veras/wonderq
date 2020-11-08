75a3b68d7bd4cb15587ef060311b87a681c4188d
George Véras Valentim <george.veras.valentim@gmail.com>
2020-11-08 02:40:04 -0300

    Refac the queue manager

--- clients/queue-manager.js
+++ clients/queue-manager.js
@@ -1,15 +1,33 @@
 import { v4 as uuidv4 } from 'uuid'
+import { messagesFlushInterval } from '../config/index.js'
 
+// This array will work as a queue for messages available to be picked.
 const messagesReady = []
+
+// This works as a helper dictionary, it grants me a O(1) search by id.
 const unacknowledgedMessages = {}
 
-// setTimeout(() => {
-//   db.first = db['202010071713'];
-//   db.last = db['202010071715'];
-// }, 
-// 1*1000);
+// This array will be ordered by a timestamp.
+const timerToRequeueBuffer = []
 
 export default timeoutMilliseconds => {
+
+  const purgeExpiredMessages = () => {
+    const timestampThreshold = Date.now() - timeoutMilliseconds
+    // A binary search would save me from having to processes and move each message one by one.
+    // The binary search has a complexity of O(log n).
+    const firstExpiredMsgIndex = binarySearchAlgorithm.getFirstExpiredIndex(timestampThreshold, timerToRequeueBuffer)
+  
+    // Again, no need to lock the array.
+    const expiredMessages = timerToRequeueBuffer.splice(0, firstExpiredMsgIndex + 1)
+    messagesReady.push(...expiredMessages)
+  
+    // Flush the helper dictionary
+    expiredMessages.forEach(msg => delete unacknowledgedMessages[msg.id])
+  }
+
+  setInterval(purgeExpiredMessages, messagesFlushInterval)
+
   return {
     pushToQueue: message => {
       const id = uuidv4()
@@ -19,12 +37,38 @@ export default timeoutMilliseconds => {
 
     pullFromQueue: () => {
       const pulledMessage = messagesReady.shift()
-      unacknowledgedMessages[pulledMessage.id] = pulledMessage.content
+      pulledMessage.timestamp = Date.now()
+
+      const index = timerToRequeueBuffer.push(pulledMessage) - 1
+      unacknowledgedMessages[pulledMessage.id] = index
       return pulledMessage
     },
 
     acknowledgeMessage: messageId => {
-      delete unacknowledgedMessages[messageId]
+      const indexToPurge = unacknowledgedMessages[messageId]
+      const messageToPurge = timerToRequeueBuffer[indexToPurge]
+      const timestampThreshold = Date.now() - timeoutMilliseconds
+
+      if(messageToPurge.id === messageId && messageToPurge.timestamp > timestampThreshold) {
+        // One doesn't need to lock the array here because javascript is not multi-threaded.
+        // Asynchronous does not mean multiple threads.
+        timerToRequeueBuffer.splice(indexToPurge, 1)
+
+        delete unacknowledgedMessages[messageId]
+        return true
+      }
+
+      return false
+    },
+
+    purgeExpiredMessages,
+
+    getQueueLength: () => {
+      return messagesReady.length
+    },
+
+    getUnackCount: () => {
+      return Object.keys(unacknowledgedMessages).length
     },
   }
 }
